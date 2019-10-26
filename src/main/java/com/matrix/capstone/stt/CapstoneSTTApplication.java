8f71ed297da091d2cb8ec5cfa0d43f163d41b841
Dheri <ps.dheri@gmail.com>
2019-10-26 01:06:59 -0400

    changed pom

--- src/main/java/com/matrix/capstone/stt/CapstoneSTTApplication.java
+++ src/main/java/com/matrix/capstone/stt/CapstoneSTTApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CapstoneKeyFeatureApplication {
+public class CapstoneSTTApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CapstoneKeyFeatureApplication.class, args);
+		SpringApplication.run(CapstoneSTTApplication.class, args);
 	}
 
 }
